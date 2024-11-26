9a162b9961ef70d3f663bdab16bb5e90e8014b30
Lee Dahee <ai04120@naver.com>
2024-11-26 14:48:02 +0900

    feat: 라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,42 @@
 import "./App.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Outlet />,
+        // errorElement: <NotFound />,
+        children: [
+            { path: "login" /* element: <Login /> */ },
+            { path: "location-select" },
+            { path: "my-location-setting" },
+            { path: "" },
+            { path: "item-view/:id" },
+            { path: "chatting-list" },
+            { path: "chatting/:id" },
+            { path: "item-register" },
+            { path: "item-register/hashtag" },
+            { path: "item-register/direct" },
+            { path: "search" },
+            { path: "search/hashtag" },
+            { path: "search-result" },
+            { path: "my-page" },
+            { path: "my-page/edit" },
+            { path: "my-page/purchase-list" },
+            { path: "my-page/sale-list" },
+        ],
+    },
+]);
 
 function App() {
     return (
         <QueryClientProvider client={queryClient}>
             <ReactQueryDevtools initialIsOpen={true} />
+            <RouterProvider router={router} />
             <div>Hello World</div>
         </QueryClientProvider>
     );
