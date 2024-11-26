02fc3fc034e8e2ba6bc9e0a1a1a3877f3efb3ccf
xjwsx <wlstmdghs@naver.com>
2024-11-26 22:31:47 +0900

    chore: 네비게이션 바 디자인 수정

--- src/components/atoms/NavigationBar.tsx
+++ src/components/atoms/NavigationBar.tsx
@@ -1,57 +1,57 @@
 import styled from "styled-components";
-// import { useState } from "react";
-// import HomeIcon from "../../assets/icons/home.svg?react";
-// import HomeActiveIcon from "../../assets/icons/homeActive.svg?react";
-// import SearchIcon from "../../assets/icons/search.svg?react";
-// import SearchActiveIcon from "../../assets/icons/searchActive.svg?react";
-// import ChatIcon from "../../assets/icons/check0.svg?react";
-// import ChatActiveIcon from "../../assets/icons/check0Active.svg?react";
-// import PetsIcon from "../../assets/icons/footPrint.svg?react";
-// import PetsActiveIcon from "../../assets/icons/footPrintActive.svg?react";
+import { useState } from "react";
+import HomeIcon from "../../assets/icons/home.svg?react";
+import HomeActiveIcon from "../../assets/icons/homeActive.svg?react";
+import SearchIcon from "../../assets/icons/search.svg?react";
+import SearchActiveIcon from "../../assets/icons/searchActive.svg?react";
+import ChatIcon from "../../assets/icons/check0.svg?react";
+import ChatActiveIcon from "../../assets/icons/check0Active.svg?react";
+import PetsIcon from "../../assets/icons/footPrint.svg?react";
+import PetsActiveIcon from "../../assets/icons/footPrintActive.svg?react";
 
-// const NavigationBar = () => {
-//     const [activeTab, setActiveTab] = useState("home");
+interface NavButtonProps {
+    activeTab: boolean;
+}
+export default function NavigationBar() {
+    const [activeTab, setActiveTab] = useState("home");
 
-//     return (
-//         <NavContainer>
-//             <NavButton activeTab={activeTab === "home"} onClick={() => setActiveTab("home")}>
-//                 {activeTab === "home" ? <HomeActiveIcon /> : <HomeIcon />}
-//                 <span>홈</span>
-//             </NavButton>
-//             <NavButton activeTab={activeTab === "search"} onClick={() => setActiveTab("search")}>
-//                 {activeTab === "search" ? <SearchActiveIcon /> : <SearchIcon />}
-//                 <span>검색</span>
-//             </NavButton>
-//             <NavButton activeTab={activeTab === "chat"} onClick={() => setActiveTab("chat")}>
-//                 {activeTab === "chat" ? <ChatActiveIcon /> : <ChatIcon />}
-//                 <span>채팅</span>
-//             </NavButton>
-//             <NavButton activeTab={activeTab === "my"} onClick={() => setActiveTab("my")}>
-//                 {activeTab === "my" ? <PetsActiveIcon /> : <PetsIcon />}
-//                 <span>마이</span>
-//             </NavButton>
-//         </NavContainer>
-//     );
-// };
+    return (
+        <NavContainer>
+            <NavButton activeTab={activeTab === "home"} onClick={() => setActiveTab("home")}>
+                {activeTab === "home" ? <HomeActiveIcon /> : <HomeIcon />}
+                <span>홈</span>
+            </NavButton>
+            <NavButton activeTab={activeTab === "search"} onClick={() => setActiveTab("search")}>
+                {activeTab === "search" ? <SearchActiveIcon /> : <SearchIcon />}
+                <span>검색</span>
+            </NavButton>
+            <NavButton activeTab={activeTab === "chat"} onClick={() => setActiveTab("chat")}>
+                {activeTab === "chat" ? <ChatActiveIcon /> : <ChatIcon />}
+                <span>채팅</span>
+            </NavButton>
+            <NavButton activeTab={activeTab === "my"} onClick={() => setActiveTab("my")}>
+                {activeTab === "my" ? <PetsActiveIcon /> : <PetsIcon />}
+                <span>마이</span>
+            </NavButton>
+        </NavContainer>
+    );
+}
 
-export const NavContainer = styled.div`
-    position: fixed;
-    bottom: 0;
-    left: 0;
-    right: 0;
+const NavContainer = styled.div`
+    width: 375px;
     height: 60px;
     background: white;
     display: flex;
     justify-content: space-around;
     align-items: center;
     border-top: 1px solid #eee;
+    position: fixed;
+    bottom: 0;
+    left: 50%;
+    transform: translateX(-50%);
 `;
 
-interface NavButtonProps {
-    activeTab: boolean;
-}
-
-export const NavButton = styled.button<NavButtonProps>`
+const NavButton = styled.button<NavButtonProps>`
     display: flex;
     flex-direction: column;
     align-items: center;
