2fbe8069272ca35138621d3a59c28558b3d43b67
Dahee Lee <ai04120@naver.com>
2024-11-28 21:14:53 +0900

    fix: 홈화면 수정중

--- src/components/layout/HomeLayout.tsx
+++ src/components/layout/HomeLayout.tsx
@@ -2,18 +2,39 @@ import HomeHeader from "@/components/atoms/HomeHeader";
 import NavigationBar from "@/components/atoms/NavigationBar";
 import { ReactNode } from "react";
 import { ItemPlusButton } from "@/components/atoms/Button";
+import { styled } from "styled-components";
 
 type HomeLayoutProps = {
     children: ReactNode;
 };
+
+const StyledDiv = styled.div`
+    width: 100%;
+    max-width: 375px;
+    margin: 0 auto;
+    position: fixed;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: 5;
+`;
+
+const BtnWrap = styled.div`
+    position: absolute;
+    width: 100%;
+    right: -290px;
+`;
 
 const HomeLayout = ({ children }: HomeLayoutProps) => {
     return (
         <>
             <HomeHeader />
             {children}
-
-            <ItemPlusButton />
+            <StyledDiv>
+                <BtnWrap>
+                    <ItemPlusButton />
+                </BtnWrap>
+            </StyledDiv>
             <NavigationBar />
         </>
     );
