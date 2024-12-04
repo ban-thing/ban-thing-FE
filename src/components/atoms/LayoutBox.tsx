3527e82478e2b7e60757f7dec025116b3b58ef94
Lee Dahee <ai04120@naver.com>
2024-12-04 11:26:44 +0900

    feat: 특정 주소에서 페이지 max width 해제

--- src/components/atoms/LayoutBox.tsx
+++ src/components/atoms/LayoutBox.tsx
@@ -1,8 +1,9 @@
 import styled from "styled-components";
 
-export const LayoutBox = styled.main`
-    max-width: 375px;
+export const LayoutBox = styled.main<{ $nullMaxWidth: boolean }>`
+    max-width: ${(props) => (props.$nullMaxWidth ? "100vw" : "375px")};
     min-height: 100vh;
     margin: 0 auto;
+    overflow-x: hidden;
     /* background-color: #ddd; */
 `;
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,9 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { LayoutBox } from "../atoms/LayoutBox";
 
 const Layout = () => {
+    const location = useLocation();
+
+    // 특정경로에서 max-width 미설정
+    // TODO: 로그인창 max width 문의
+    const pagesWithoutMaxWidth =
+        location.pathname.startsWith("/login") || location.pathname.startsWith("/item-register");
+
     return (
-        <LayoutBox>
+        <LayoutBox $nullMaxWidth={pagesWithoutMaxWidth}>
             <Outlet />
         </LayoutBox>
     );
