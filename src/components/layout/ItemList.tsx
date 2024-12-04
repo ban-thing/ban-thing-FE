f27d7c2ed64aef603d9ed8330b447013fb7141a3
Dahee Lee <ai04120@naver.com>
2024-12-04 22:14:41 +0900

    fix: 홈화면 데이터 없을떄 화면 추가

--- src/components/layout/ItemList.tsx
+++ src/components/layout/ItemList.tsx
@@ -1,8 +1,10 @@
 import styled from "styled-components";
 import ItemContainer from "@/components/molecules/ItemContainer";
+import NoItemInList from "@/components/molecules/ItemView/noItemInList";
 import { dummyItemList } from "@/store/ITemListDummyData";
 
-const StyledItemList = styled.div`
+const StyledItemList = styled.div<{ height: string }>`
+    height: ${({ height }) => (height ? height : null)};
     width: 100%;
     padding: 50px 20px 60px;
     box-sizing: border-box;
@@ -14,19 +16,23 @@ const ItemList = () => {
     // 선택된 주소로 아이템 리스트 필터링
     // const { currentLocation } = useItemListLocationStore();
     return (
-        <StyledItemList>
-            {dummyItemList.map((item, index) => (
-                <ItemContainer
-                    key={index}
-                    imgUrl={item.imgUrl}
-                    itemId={item.itemId}
-                    title={item.title}
-                    price={item.price}
-                    address={item.address}
-                    updatedAt={item.updatedAt}
-                    type={item.type}
-                />
-            ))}
+        <StyledItemList height={!dummyItemList.length ? "100vh" : ""}>
+            {dummyItemList.length ? (
+                dummyItemList?.map((item, index) => (
+                    <ItemContainer
+                        key={index}
+                        imgUrl={item.imgUrl}
+                        itemId={item.itemId}
+                        title={item.title}
+                        price={item.price}
+                        address={item.address}
+                        updatedAt={item.updatedAt}
+                        type={item.type}
+                    />
+                ))
+            ) : (
+                <NoItemInList />
+            )}
         </StyledItemList>
     );
 };
