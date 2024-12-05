75ae89d5d24ab6a845d04c47b77e345dead894ca
xjwsx <wlstmdghs@naver.com>
2024-12-06 00:05:55 +0900

    chore: cicd 서버 연결 코드 추가

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,11 @@ export default defineConfig({
         },
     },
     server: {
+        host: true,
+        port: 5173,
+        watch: {
+            usePolling: true,
+        },
         proxy: {
             "/api": {
                 target: "https://api.vworld.kr/ned/data",
