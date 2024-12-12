f425b5b19887291cf2e2d0feb1765d31a20e91d9
xjwsx <wlstmdghs@naver.com>
2024-12-13 00:43:59 +0900

    feat: 채팅 타입 설정

--- src/types/Chat.ts
+++ src/types/Chat.ts
@@ -1,11 +1,29 @@
 // 채팅 목록 조회
 export type ChatList = {
     chatRoomId: number;
+    address: string;
     profileImg: string;
     nickname: string;
     latestMessage: string;
     latestMessageDateTime: Date;
     unreadMessageCount: number;
-    address: string;
     type: string; // 판매/구매
+};
+// 채팅방 생성
+export type ChatRoomCreate = {
+    sellerId: number;
+    itemId: number;
+};
+// 채팅 대화 내용, 아이템 정보 조회
+export type ChatRoomView = {
+    itemId: number;
+    title: string;
+    price: number;
+    itemImage: string[];
+    messages: {
+        senderId: number;
+        message: string;
+        time: Date;
+    }[];
+    hasNext: boolean;
 };
