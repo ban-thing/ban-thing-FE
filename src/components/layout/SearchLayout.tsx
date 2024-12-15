599d6874b84bb1d4313cbcb110b342e38c46a673
Dahee Lee <ai04120@naver.com>
2024-12-15 14:53:40 +0900

    fix: 빌드오류

--- src/components/layout/SearchLayout.tsx
+++ src/components/layout/SearchLayout.tsx
@@ -25,6 +25,8 @@ export default function SearchLayout({ children }: { children: React.ReactNode }
         }
     };
 
+    console.log(children);
+
     return (
         <SearchContainer>
             <SearchHeader>
