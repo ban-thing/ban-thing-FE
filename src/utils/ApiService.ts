42027d76549727af90969c01b2630c46ee26202f
Jeahyuk Jeong <jeong_j1@denison.edu>
2024-12-19 11:04:28 +0900

    [fix] 서버 http로 복구

--- src/utils/ApiService.ts
+++ src/utils/ApiService.ts
@@ -11,7 +11,7 @@ export default class ApiService {
 
     constructor() {
         const config: AxiosRequestConfig = {
-            baseURL: `https://211.188.62.82:8080`,
+            baseURL: `http://211.188.62.82:8080`,
         };
 
         this.axiosInstance = axios.create(config);
