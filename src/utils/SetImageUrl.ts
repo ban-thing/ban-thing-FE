3fb15e7e35bbb62090373f6eca2f2a9c508515c1
Jeahyuk Jeong <jeong_j1@denison.edu>
2024-12-24 01:52:53 +0900

    [test] 상품 수정 관련 테스트

--- src/utils/SetImageUrl.ts
+++ src/utils/SetImageUrl.ts
@@ -39,6 +39,11 @@ export const getFileFromUrl = async (id: string | number, name: string) => {
           "Access-Control-Allow_origin": "https://kr.object.ncloudstorage.com"  
         },
     });
+
+    if (!response.ok) {
+        throw new Error(`Failed to fetch file: ${response.statusText}`);
+    }
+    
     const blob = await response.blob();
     return new File([blob], `${data}.${type}`, { type: blob.type });
 };
