530081b3196a276da60d67d648aeb024c8527d1a
xjwsx <wlstmdghs@naver.com>
2025-02-18 16:35:02 +0900

    fix: 회원 탈퇴 api 수정중

--- src/hooks/api/UsersQuery.ts
+++ src/hooks/api/UsersQuery.ts
@@ -100,12 +100,7 @@ export const useFetchDeleteUser = () => {
     const navigate = useNavigate();
     return useMutation({
         mutationFn: async (reason: string) => {
-            return await apiService.delete<any>(
-                `my/delete?reason=${encodeURIComponent(reason)}`,
-                {}, // empty body
-                "",
-                true,
-            );
+            return await apiService.delete<any>(`my/delete`, { reason }, "", true);
         },
         onError: (error, variables, context) => {
             console.log(error, variables, context);
