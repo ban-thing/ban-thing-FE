65d79547d88d6af41f115140e375c91475b5f00e
Woo Sung jin <woosungjin@Wooui-MacBookPro-2.local>
2025-03-26 21:09:38 +0900

    feat: 나의 찜 페이지 api 연결

--- src/pages/MyPage/MyPageFavoriteList .tsx
+++ src/pages/MyPage/MyPageFavoriteList .tsx
@@ -1,37 +1,22 @@
 import styled from "styled-components";
 import ItemList from "@/components/layout/ItemList";
-import { useFetchMyPurchases, useFetchMySales } from "@/hooks/api/ItemsQuery";
-import { useLocation } from "react-router-dom";
+import { useFetchWishlist } from "@/hooks/api/UsersQuery";
 import { PageTitleWithBackButton } from "@/components/atoms/PageTitle";
 
 const MyPageFavoriteList = () => {
-    const { pathname } = useLocation();
-    const isSalePage = pathname.includes("sale");
-    const { data: purchasesData, isLoading: isLoading1 } = useFetchMyPurchases();
-    const { data: saleData, isLoading: isLoading2 } = useFetchMySales();
+    const { data: wishlistData, isLoading } = useFetchWishlist();
 
     return (
         <MyPageItemListWrap>
             <PageTitleWithBackButton text="나의 찜" $margin="10px 0" backTo="/my-page" />
-            {isSalePage ? (
-                <ItemList
-                    data={saleData?.data}
-                    isLoading={isLoading2}
-                    padding="0 20px"
-                    viewEditButton={true}
-                    noItemText="앗! 아직 내역이 없어요."
-                    isMyPage={true}
-                />
-            ) : (
-                <ItemList
-                    data={purchasesData?.data}
-                    isLoading={isLoading1}
-                    padding="0 20px"
-                    viewEditButton={false}
-                    noItemText="앗! 아직 내역이 없어요."
-                    isMyPage={true}
-                />
-            )}
+            <ItemList
+                data={wishlistData?.data}
+                isLoading={isLoading}
+                padding="0 20px"
+                viewEditButton={false}
+                noItemText="앗! 아직 찜한 상품이 없어요."
+                isMyPage={true}
+            />
         </MyPageItemListWrap>
     );
 };
