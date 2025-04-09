0a0718c32c6e94f278a49f710bf5a2f718f0947d
xjwsx <wlstmdghs@naver.com>
2025-04-09 23:47:39 +0900

    fix: 헤더 그림자 효과 추가

--- src/components/atoms/HomeHeader.tsx
+++ src/components/atoms/HomeHeader.tsx
@@ -19,8 +19,7 @@ const HeaderBox = styled.header`
     position: fixed;
     top: 0;
     z-index: 15;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-    overflow: hidden;
+    box-shadow: 0 1px 2px -1px rgba(0, 0, 0, 0.1);
 `;
 
 const SearchButton = styled.button`
