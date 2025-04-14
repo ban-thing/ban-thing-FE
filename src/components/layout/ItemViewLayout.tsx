e0b273ceec73dd498feee1feed5e969f01022800
xjwsx <wlstmdghs@naver.com>
2025-04-14 23:56:39 +0900

    fix: ui 수정

--- src/components/layout/ItemViewLayout.tsx
+++ src/components/layout/ItemViewLayout.tsx
@@ -1,9 +1,5 @@
 import ItemViewBottomBar from "@/components/molecules/ItemView/ItemViewBottomBar";
 import { ReactNode } from "react";
-import styled from "styled-components";
-import BackIcon from "@/assets/icons/back.svg?react";
-import HomeIcon from "@/assets/icons/home.svg?react";
-import SearchIcon from "@/assets/icons/search.svg?react";
 
 type ItemViewLayoutProps = {
     children: ReactNode;
@@ -33,26 +29,10 @@ const ItemViewLayout = ({
     status,
     wishlisted = false,
     wishlistCount = 0,
-    onClickBack,
-    onClickHome,
-    onClickSearch,
     onWishlistClick,
 }: ItemViewLayoutProps) => {
     return (
         <>
-            <ItemViewHeader>
-                <BackButton onClick={onClickBack}>
-                    <BackIcon />
-                </BackButton>
-                <HeaderButtonGroup>
-                    <HomeButton onClick={onClickHome}>
-                        <HomeIcon />
-                    </HomeButton>
-                    <SearchButton onClick={onClickSearch}>
-                        <SearchIcon />
-                    </SearchButton>
-                </HeaderButtonGroup>
-            </ItemViewHeader>
             {children}
             <ItemViewBottomBar
                 type={type}
@@ -70,35 +50,3 @@ const ItemViewLayout = ({
 };
 
 export default ItemViewLayout;
-
-const ItemViewHeader = styled.header`
-    height: 50px;
-    width: 100%;
-    max-width: 375px;
-    padding: 14px 20px;
-    box-sizing: border-box;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    background-color: #fff;
-    position: fixed;
-    top: 0;
-    z-index: 15;
-`;
-
-const HeaderButtonGroup = styled.div`
-    display: flex;
-    gap: 16px;
-`;
-
-const BackButton = styled.button`
-    padding: 0;
-`;
-
-const HomeButton = styled.button`
-    padding: 0;
-`;
-
-const SearchButton = styled.button`
-    padding: 0;
-`;
